e7ae4d8dd786621a159117e8cd12de0194bbb145
ronaldperaza <ronaldperaza460@gmail.com>
2020-05-25 22:17:49 -0400

    firts commit

---
+++ poll_project/poll/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .forms import CreatePollForm
+
+
+def home(request):
+    context = {}
+    return render (request, 'poll/home.html', context)
+
+def create(request):
+    
+    if request.method == 'POST':
+        form = CreatePollForm(request.POST)
+    else:
+        form = CreatePollForm()   
+    
+    context = {'form': form}
+    return render (request, 'poll/create.html', context)
+
+def results(request):
+    context = {}
+    return render (request, 'poll/results.html', context)
+
+def vote(request):
+    context = {}
+    return render (request, 'poll/vote.html', context)
